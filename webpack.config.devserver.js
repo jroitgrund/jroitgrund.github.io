0f3112d904055124567a028653b82a30f296ec82
Jonathan Roitgrund <jroitgrund@palantir.com>
2017-02-09 12:40:21 +0000

    Remove build artifacts

--- webpack.config.devserver.js
+++
@@ -1,6 +0,0 @@
-const config = Object.assign({}, require("./webpack.config.js"));
-
-config.output.publicPath = "http://localhost:8080/"
-config.devtool = "inline-sourcemap",
-
-module.exports = config;
