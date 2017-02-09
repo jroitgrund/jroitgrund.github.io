d2e19d588a8561290a9843f18f9a275a2b0b129a
Jonathan Roitgrund <jroitgrund@palantir.com>
2017-02-09 14:07:01 +0000

    Add 'hack the planet' feature

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 require("./style");
+
+const HACK_THE_PLANET_LABEL = "h4cktheplanet!";
+
+let hacked = false;
+const hackThePlanet = document.createElement("a");
+hackThePlanet.className = "hacktheplanet";
+const body = document.getElementsByTagName("body")[0];
+hackThePlanet.innerText = HACK_THE_PLANET_LABEL;
+
+hackThePlanet.onclick = () => {
+    if (hacked) {
+        body.className = body.className.replace( /(?:^|\s)hacked(?!\S)/g , '' );
+        hacked = false;
+        hackThePlanet.innerText = HACK_THE_PLANET_LABEL;
+    } else {
+        body.className += " hacked";
+        hackThePlanet.innerText = "unh4cktheplanet!";
+        hacked = true;
+    }
+}
+
+document.getElementById("header").appendChild(hackThePlanet);
