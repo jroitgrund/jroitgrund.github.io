5c8097924e218517d5a84d51ad0ddf8851ae539f
Jonathan Roitgrund <jroitgrund@palantir.com>
2017-02-09 12:40:32 +0000

    Use existing repo on Circle

--- deploy-github.sh
+++ deploy-github.sh
@@ -3,11 +3,8 @@
 set -ex
 cd "$(dirname "$0")"
 
-remote=$(git config remote.origin.url)
-cd ~
 git config --global user.email bot@circleci.com > /dev/null 2>&1
 git config --global user.name CircleCI > /dev/null 2>&1
-git clone $remote
 
 git checkout master
 git merge develop
