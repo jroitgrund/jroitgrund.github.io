966d2a4debc3917e4849cacfb1c2ffe317578340
jroitgrund <jroitgrund@gmail.com>
2017-02-09 13:19:11 +0000

    Reset to develop rather than merging

--- deploy-github.sh
+++ deploy-github.sh
@@ -9,7 +9,7 @@ git config --global user.name CircleCI > /dev/null 2>&1
 git reset --hard HEAD
 git clean -df
 git checkout master
-git merge develop
+git reset --hard develop
 yarn install
 yarn run build
 git commit -am "Deploy to GitHub pages"
