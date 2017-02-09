87c6a7e349c20337c0c2aa58d6ae3fdefb010b9e
Jonathan Roitgrund <jroitgrund@palantir.com>
2017-02-09 14:40:59 +0000

    Fix path for remove-script-tag

--- remove-script-tag.sh
+++ remove-script-tag.sh
@@ -4,4 +4,4 @@ set -ex
 
 cd "$(dirname "$0")"
 
-sed -i "" 's:<script.*</script>::g' 404.html || sed -i 's:<script.*</script>::g' 404.html
+sed -i "" 's:<script.*</script>::g' 404/index.html || sed -i 's:<script.*</script>::g' 404/index.html
